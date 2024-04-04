fa51b737e00c814d069e6e798f43ce89e586e03b
Vadim Zharov <vadim.zharov@spectrocloud.com>
2024-04-04 23:34:29 +0000

    backend config adjusted

--- simple-app-manifests-profile/backend.tf
+++ simple-app-manifests-profile/backend.tf
@@ -1,4 +1,12 @@
 terraform {
   backend "s3" {
+    key                         = "simple-app.tfstate"
+    region                      = "main"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+
   }
 }
